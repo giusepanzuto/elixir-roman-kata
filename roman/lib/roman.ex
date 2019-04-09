a0c8a954dda9f26af0758623e09a51c0db5ab35e
Giuseppe Panzuto <giusepanzuto@gmail.com>
2019-04-09 23:06:02 +0200

    it works

--- roman/lib/roman.ex
+++ roman/lib/roman.ex
@@ -2,17 +2,36 @@ defmodule ROMAN do
   @moduledoc """
   Documentation for ROMAN.
   """
+  @numerals [{10, "X"}, {9, "IX"}, {5, "V"}, {4, "IV"}, {1, "I"}]
 
-  @doc """
-  Hello world.
+  @doc ~S"""
+  Converts arabic to roman number
 
   ## Examples
-
-      iex> ROMAN.hello()
-      :world
-
+    iex> ROMAN.converts(0)
+    ""
+    iex> ROMAN.converts(1)
+    "I"
+    iex> ROMAN.converts(2)
+    "II"
+    iex> ROMAN.converts(3)
+    "III"
+    iex> ROMAN.converts(4)
+    "IV"
+    iex> ROMAN.converts(5)
+    "V"
+    iex> ROMAN.converts(6)
+    "VI"
+    iex> ROMAN.converts(9)
+    "IX"
+    iex> ROMAN.converts(10)
+    "X"
+    iex> ROMAN.converts(28)
+    "XXVIII"
   """
-  def hello do
-    :world
-  end
+  def converts(number), do: converts(number, @numerals)
+  defp converts(number, _) when number < 1, do: ""
+  defp converts(number, [{arabic,roman}|_]) when number >= arabic, do: roman <> converts(number - arabic)
+  defp converts(number, [_|tail]), do: converts(number, tail)
+
 end
--- roman/test/roman_test.exs
+++ roman/test/roman_test.exs
@@ -1,8 +1,4 @@
 defmodule ROMANTest do
   use ExUnit.Case
   doctest ROMAN
-
-  test "greets the world" do
-    assert ROMAN.hello() == :world
-  end
 end
