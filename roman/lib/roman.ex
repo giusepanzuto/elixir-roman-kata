4e53dc85d58c7cd40f5bf1ff2d8ddd7d5cb78db8
Giuseppe Panzuto <giusepanzuto@gmail.com>
2019-04-10 00:24:51 +0200

    parse implemented

--- roman/lib/roman.ex
+++ roman/lib/roman.ex
@@ -34,4 +34,42 @@ defmodule ROMAN do
   defp converts(number, [{arabic,roman}|_]) when number >= arabic, do: roman <> converts(number - arabic)
   defp converts(number, [_|tail]), do: converts(number, tail)
 
+  @doc ~S"""
+  Parse roman number to arabic
+
+  ## Examples
+    iex> ROMAN.parse("")
+    0
+    iex> ROMAN.parse("I")
+    1
+    iex> ROMAN.parse("II")
+    2
+    iex> ROMAN.parse("III")
+    3
+    iex> ROMAN.parse("IV")
+    4
+    iex> ROMAN.parse("V")
+    5
+    iex> ROMAN.parse("VI")
+    6
+    iex> ROMAN.parse("IX")
+    9
+    iex> ROMAN.parse("X")
+    10
+    iex> ROMAN.parse("XXVIII")
+    28
+  """
+  def parse(number), do: parse(number,0)
+  defp parse(number,accumulator), do: parse(number,accumulator,Enum.reverse(@numerals))
+  defp parse(_,accumulator,[]), do: accumulator
+  defp parse(number,accumulator,[{_,roman}|tail])
+    when byte_size(number) < byte_size(roman),
+    do: parse(number,accumulator, tail)
+  defp parse(number,accumulator,[{arabic,roman}|tail]) do
+    cond do
+      String.slice(number, String.length(number) - String.length(roman), String.length(roman)) == roman -> parse(String.slice(number, 0, String.length(number)-String.length(roman)),accumulator+arabic)
+      true -> parse(number,accumulator,tail)
+    end
+  end
+#  defp parse(number,accumulator,[_|tail]), do: parse(number,accumulator,tail)
 end
--- roman/test/roman_test.exs
+++ roman/test/roman_test.exs
@@ -1,4 +1,13 @@
 defmodule ROMANTest do
   use ExUnit.Case
   doctest ROMAN
+
+  test "Convert->Parse tests", do: convertParseTests()
+
+  def convertParseTests, do: convertParseTests(0)
+  defp convertParseTests(testCase) when testCase < 30 do
+    assert ROMAN.parse(ROMAN.converts(testCase)) == testCase
+    convertParseTests(testCase+1)
+  end
+  defp convertParseTests(testCase), do: assert ROMAN.parse(ROMAN.converts(testCase)) == testCase
 end
